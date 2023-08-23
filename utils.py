279d455ad65a076299d9dd8ef70029a008156fac
Andrew McLaughlin <andrewmcl6081@gmail.com>
2023-08-23 11:47:05 -0500

    added base64 encoding function required for auth header

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import random
 import string
+import base64
 
 # Random string between 43 to 128 characters
 def gen_random_string(length):
@@ -10,3 +11,11 @@ def gen_random_string(length):
         text += random.choice(possible)
     
     return text
+
+def get_auth_header(client_id, client_secret):
+    auth = client_id + ":" + client_secret
+    auth_bytes = auth.encode("ascii")
+    base64_bytes = base64.b64encode(auth_bytes)
+    base64_auth = base64_bytes.decode("ascii")
+    
+    return base64_auth
