21f747936f156be1e86b82e286e242eb62d33a08
Andrew McLaughlin <andrewmcl6081@gmail.com>
2023-09-02 15:36:48 -0500

    modiefied function arguments used to construct request url

--- queries.py
+++ queries.py
@@ -2,8 +2,8 @@ from utils import get_auth_header
 from requests import get
 import json
 
-def get_user_top_tracks(token, type, time_range):
-    url = f"https://api.spotify.com/v1/me/top/{type}?time_range={time_range}&limit=5"
+def get_user_top_tracks(token, time_range):
+    url = f"https://api.spotify.com/v1/me/top/tracks?time_range={time_range}&limit=5"
     print(url)
     header = get_auth_header(token)
     
