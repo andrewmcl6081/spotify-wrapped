d97ac81c1849d591b6d9762bcd8aa25b222bb600
Andrew McLaughlin <andrewmcl6081@gmail.com>
2023-09-03 13:25:26 -0500

    added functionality for getting user's top artists

--- queries.py
+++ queries.py
@@ -1,4 +1,4 @@
-from utils import get_auth_header, filter_tracks
+from utils import get_auth_header, filter_tracks, filter_artists
 from requests import get
 import json
 
@@ -13,3 +13,14 @@ def get_user_top_tracks(token, time_range):
     
     
     return filtered_tracks
+
+def get_user_top_artists(token, time_range):
+    url = f"https://api.spotify.com/v1/me/top/artists?time_range={time_range}&limit=5"
+    header = get_auth_header(token)
+    
+    response = get(url, headers=header)
+    data = json.loads(response.text)
+    
+    filtered_artists = filter_artists(data["items"])
+    
+    return filtered_artists
