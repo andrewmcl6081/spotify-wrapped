85969e591111f42c59e28caf1107b45c62a26d91
haydencj <haydenjohns02@gmail.com>
2023-09-03 18:29:45 -0500

    Router set up

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Home() {
+    return (
+        // fractal
+        <> 
+            <Header />
+            <h2>Home</h2>
+        </>
+    )
+}
---
+++ client/src/pages/TopTracks.js
@@ -0,0 +1,11 @@
+import Header from '../components/Header'
+import React, {useState, useEffect} from 'react'
+
+export default function TopTracks() {
+    return (
+        <>
+            <Header />
+            <h2>Top Tracks</h2>
+        </>
+    )
+}
