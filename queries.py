bb949c5d2bd91d0cc24d4f681200dbf53d6091cf
Andrew McLaughlin <andrewmcl6081@gmail.com>
2023-09-02 23:03:37 -0500

    removed track filtering from function where api call takes place

--- queries.py
+++ queries.py
@@ -1,33 +1,15 @@
-from utils import get_auth_header
+from utils import get_auth_header, filter_tracks
 from requests import get
 import json
 
 def get_user_top_tracks(token, time_range):
     url = f"https://api.spotify.com/v1/me/top/tracks?time_range={time_range}&limit=5"
-    print(url)
     header = get_auth_header(token)
     
     response = get(url, headers=header)
     data = json.loads(response.text)
     
+    filtered_tracks = filter_tracks(data["items"])
     
-    data = data["items"]
-    filtered_data = []
     
-    for track in data:
-        
-        extracted_track_data = {
-            "album_name" : track["album"]["name"],
-            "album_images" : track["album"]["images"],
-            "track_name" : track["name"],
-            "track_id" : track["id"],
-            "track_artists" : []
-        }
-        
-        for artist in track["artists"]:
-            extracted_track_data["track_artists"].append(artist["name"])
-        
-        filtered_data.append(extracted_track_data)
-        
-    
-    return filtered_data
+    return filtered_tracks
