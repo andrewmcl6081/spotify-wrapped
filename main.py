b471f293a41205a6b9f8aca991fff5b7de81aa8f
Andrew McLaughlin <andrewmcl6081@gmail.com>
2023-10-15 20:46:58 -0500

    implemented use of cookies

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request, session, url_for
+from flask import Flask, redirect, request, session, make_response
 from flask_session import Session
 from flask_cors import CORS
 from utils import gen_random_string, get_tokens
@@ -10,18 +10,18 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
+CORS(app, origins="http://localhost:5173", supports_credentials=True)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.secret_key = "spotify_wrapped"
-
+app.config["CORS_ALLOW_CREDENTIALS"] = True
+Session(app)
 
 #Globals
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = "http://localhost:8080/api/callback"
-SCOPE = "user-library-read user-read-recently-played user-top-read user-follow-read"
+SCOPE = "user-library-read user-read-recently-played user-top-read user-follow-read user-read-email"
 
 
 @app.route("/api/login", methods=["GET"])
@@ -47,7 +47,7 @@ def authorize_spotify():
 
 @app.route("/api/callback", methods=["GET"])
 def account_page():
-        
+    
     code = request.args.get("code")
     state = request.args.get("state")
     stored_state = session.get("state")  
@@ -56,45 +56,38 @@ def account_page():
     if state is None or state != stored_state:
         return "<p>Unauthorized</p>"
     
-    print("State from /callback", state)
+    access_token, refresh_token = get_tokens(code)
     
-    session["access_tokens"] = get_tokens(code)
-    print("token from /analytics:", session["access_tokens"][0])
+    response = make_response(redirect("http://localhost:5173/tracks"))
+    response.set_cookie("access_token", access_token, httponly=True)
+    response.set_cookie("refresh_token", refresh_token, httponly=True)
     
-    return redirect("http://localhost:5173/analytics")
-
-
-# @app.route("/main")
-# def main():
-#     access_token = session.get("access_tokens")
-#     print("access token from /main", access_token[0])
-    
-#     return "<p>Welcome</p>"
-
+    return response
 
 @app.route("/api/top-tracks", methods=["GET"])
 def get_top_tracks():
-    access_tokens = session.get("access_tokens")
-    print("token from /top-tracks", access_tokens)
     
-    if not access_tokens:
+    access_token = request.cookies.get("access_token")
+    print("token from /top-tracks", access_token)
+    
+    if not access_token:
         return "<p>Not Authorized</p>"
     
-    token = access_tokens[0]
-    top_tracks = queries.get_user_top_tracks(token, "short_term")
+    
+    top_tracks = queries.get_user_top_tracks(access_token, "short_term")
     
     return top_tracks
 
 
 @app.route("/api/top-artists", methods=["GET"])
 def get_top_artists():
-    access_tokens = session.get("access_tokens")
+    access_token = request.cookies.get("access_token")
     
-    if not access_tokens:
+    if not access_token:
         return "<p>Not Authorized</p>"
     
-    token = access_tokens[0]
-    top_artists = queries.get_user_top_artists(token, "short_term")
+    
+    top_artists = queries.get_user_top_artists(access_token, "short_term")
     
     return top_artists
     
