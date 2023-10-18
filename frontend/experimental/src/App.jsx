97554ed621cc2e4e3e0a2cbd269c91b5b9dbb024
Andrew McLaughlin <andrewmcl6081@gmail.com>
2023-10-18 12:54:35 -0500

    added react-bootstrap and implemented a navbar

--- frontend/experimental/src/App.jsx
+++ frontend/experimental/src/App.jsx
@@ -1,21 +1,35 @@
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes, useNavigate } from 'react-router-dom'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
 import Analytics from './components/pages/Analytics'
 import Tracks from './components/pages/Tracks'
 import Artists from './components/pages/Artists'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/custom-styles.css'
+import { useEffect } from 'react'
 
 const App = () => {
 
     return (
         <>
-            <nav>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/about'>About</Link></li>
-                    <li><Link to='/analytics'>Analytics</Link></li>
-                </ul>
-            </nav>
+            <Navbar expand='lg' className='bg-body-tertiary'>
+                <Container>
+                    <Navbar.Brand as={Link} to='/'>Wrapify</Navbar.Brand>
+                    <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                    <Navbar.Collapse id='responsive-navbar-nav'>
+                        <Nav className='me-auto'>
+                            <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                            <Nav.Link as={Link} to='/about'>About</Nav.Link>
+                            <NavDropdown title='Analytics' id='collapsible-nav-dropdown'>
+                                <NavDropdown.Item as={Link} to='/analytics/top-tracks'>Top Tracks</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to='/analytics/top-artists'>Top Artists</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            
             <Routes>
                 <Route path='/' element={<Home />}></Route>
                 <Route path='/about' element={<About />}></Route>
