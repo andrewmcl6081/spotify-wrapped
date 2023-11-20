24fa6556208fded649c50c50f521f9f0b7889449
Andrew McLaughlin <andrewmcl6081@gmail.com>
2023-11-20 15:20:31 -0600

    removed RedirectButton component and fixed href for button in Home.jsx

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,32 +1,13 @@
-import { useState, useEffect } from 'react'
-import RedirectButton from '../RedirectButton'
-import Stack from '@mui/material/Stack'
 import Button from '@mui/material/Button'
 
 const Home = () => {
-    const [data, setData] = useState(null)
-    const [data2, setData2] = useState(null)
 
-    useEffect(() => {
-        fetch('/api')
-            .then((res) => res.json())
-            .then((data) => setData(data.message))
-            .catch((error) => console.log(error))
-    }, [])
-
-    return (
-        <>
-            <h1>Home Page</h1>
-            <RedirectButton to='http://localhost:8080/api/login'/>
-            <Stack spacing={2} direction='row'>
-                <Button variant='text'>Text</Button>
-                <Button variant='contained'>Contained</Button>
-                <Button variant='outlined'>Outlined</Button>
-            </Stack>
-            <p>{!data ? "Loading..." : data}</p>
-            <p>{!data ? "Loading Data 2..." : data2}</p>
-        </>
-    )
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Button href='/api/auth/login' variant='contained'>Authorize Spotify</Button>
+    </>
+  )
 }
 
 export default Home
