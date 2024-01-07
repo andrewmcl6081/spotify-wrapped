5da584714bfbe907ff9fea6717d25bafac5c84fa
Andrew McLaughlin <andrewmcl6081@gmail.com>
2024-01-07 13:05:29 -0600

    modified frontend api service to use cookies instead of jwt

--- frontend/src/services/artists.js
+++ frontend/src/services/artists.js
@@ -1,27 +1,17 @@
 import axios from 'axios'
 
 const getTopArtists = async (time_range) => {
-    const url = `http://localhost:8080/api/top-artists/${time_range}`
-    const jwt = localStorage.getItem('jwt')
+  const url = `/api/query/top-artists/${time_range}`
 
-    if(!jwt) {
-        throw new Error('JWT not found in local storage')
-    }
-
-    const config = {
-        headers: {
-            Authorization: `Bearer ${jwt}`,
-        },
-    }
-
-    try {
-        const response = await axios.get(`/api/query/top-artists/${time_range}`, config)
-        return response.data
-    }
-    catch (error) {
-        console.log(error)
-        throw error
-    }
+  try {
+    const response = await axios.get(url, { withCredentials: true })
+    console.log(response)
+    return response.data
+  }
+  catch (error) {
+    console.log(error)
+    throw error
+  }
 }
 
 export default { getTopArtists }
--- frontend/src/services/tracks.js
+++ frontend/src/services/tracks.js
@@ -1,27 +1,17 @@
 import axios from 'axios'
 
 const getTopTracks = async (time_range) => {
-    const url = `http://localhost:8080/api/top-tracks/${time_range}`
-    const jwt = localStorage.getItem('jwt')
+  const url = `/api/query/top-tracks/${time_range}`
 
-    if(!jwt) {
-        throw new Error('JWT not found in local storage')
-    }
-    
-    const config = {
-        headers: {
-            Authorization: `Bearer ${jwt}`,
-        },
-    }
-
-    try {
-        const response = await axios.get(`/api/query/top-tracks/${time_range}`, config)
-        return response.data
-    }
-    catch (error) {
-        console.log(error)
-        throw error
-    }
+  try {
+    const response = await axios.get(url, { withCredentials: true })
+    console.log(response)
+    return response.data
+  }
+  catch (error) {
+    console.log(error)
+    throw error
+  }
 }
 
 export default { getTopTracks }
